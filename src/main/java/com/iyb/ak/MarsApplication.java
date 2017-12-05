b510a4112990bc20b2908cdb75cdce9f301e87c4
admin801122 <434329122@qq.com>
2017-12-05 10:13:01 +0800

    集成配置中心

--- src/main/java/com/iyb/ak/MarsApplication.java
+++ src/main/java/com/iyb/ak/MarsApplication.java
@@ -1,5 +1,7 @@
 package com.iyb.ak;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,10 +10,17 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RefreshScope
-public class MarsApplication {
+//@RefreshScope
+@Slf4j
+public class MarsApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MarsApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info(">>>>>>>>>>>>>>> sc-mars 启动完成<<<<<<<<<<<<<");
 	}
 }
